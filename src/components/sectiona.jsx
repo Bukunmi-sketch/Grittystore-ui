28ed9d837648937058eec0e000b027c1ae944a96
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-03-15 09:05:38 +0100

    second section styled
    second section styled

--- src/components/sectiona.jsx
+++ src/components/sectiona.jsx
@@ -26,31 +26,31 @@ function Sectiona() {
             </div>             
         </section>
 
-        <section className="greeny-order">
+        <section className="second-section">
              <h3> We Serve You The Best Product At Best Prices </h3>
              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam molestias officia commodi. Nemo maiores culpa saepe aut, debitis sunt aspernatur vel error repellendus suscipit, necessitatibus expedita? Repellendus mollitia a totam.</p>
 
              <div className="flex-container">
                 <div className="box">
-                    <img src={first} alt="" />
-                    <p>Free delivery</p>
+                   
+                    <p>Sneakers</p>
                     <p>lorem ipsum dolor sit amet,</p>
                 </div>
 
                 <div className="box">
-                    <img src={first} alt="" />
-                    <p>Secured Payments</p>
+                   
+                    <p>Womens shoes</p>
                     <p>lorem ipsum dolor sit amet,</p>
                 </div>
 
                 <div className="box">
-                    <img src={first} alt="" />
-                    <p>24/7 Online Support</p>
+                   
+                    <p>Mens Shoes</p>
                     <p>lorem ipsum dolor sit amet,</p>
                 </div>
 
                 <div className="box">
-                    <img src={first} alt="" />
+                   
                     <p>exchange</p>
                     <p>lorem ipsum dolor sit amet,</p>
                 </div>
